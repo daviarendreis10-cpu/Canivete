d683a964e439e8d43c4770906c3876c595e51851
daviarendreis10-cpu <daviarendreis10@gmail.com>
2026-08-27 18:53:25 -0300

    Hub finalizado

--- Canivete-React/Canivete-React/src/main.tsx
+++ Canivete-React/Canivete-React/src/main.tsx
@@ -1,17 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Theme } from '@radix-ui/themes'
 import "@radix-ui/themes/styles.css";
 import { RouterProvider } from 'react-router-dom'
 // router.jsx does not currently provide TypeScript declarations.
 // @ts-expect-error The JavaScript module is valid at runtime.
 import router from './router'
 
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <Theme appearance='dark'>
         <RouterProvider router={router}/>
-      </Theme>
   </StrictMode>,
 )
